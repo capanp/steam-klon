7b68c60d71374159cb35f504f32f161f6554db19
capan <caganpinarr2003@gmail.com>
2025-03-22 16:40:49 +0300

    metadata test

--- app/app/Sekiro/page.jsx
+++ app/app/Sekiro/page.jsx
@@ -14,15 +14,12 @@ export const metadata = {
     site: 'Steam - Klon',
     title: 'Sekiro™: Shadows Die Twice - GOTY Edition',
     description: 'Sekiro™: Shadows Die Twice ile intikam yolculuğuna çık. Efsanevi bir aksiyon oyunu seni bekliyor!',
-    images: ['https://store.steampowered.com/path-to-image.jpg'],
-    type: 'product',
-    price: {
-      amount: '59.99',
-      currency: 'USD'
-    },
-    rating: '4.5',
-    reviewCount: '230336',
+    images: ['https://cdn.discordapp.com/attachments/592060301579517953/1352995045544038470/image.png'],
   },
+  priceCurrency: 'USD',
+  priceAmount: '59.99',
+  rating: '4.5',
+  reviewCount: '230336',
 };
 
 const StoreApp = () => {
