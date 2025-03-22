d4d2a43ba99d0b9f38dc399d875f1ff973181d5e
capan <caganpinarr2003@gmail.com>
2025-03-22 18:37:42 +0300

    runtime test

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,9 @@ const nextConfig = {
   images: {
     unoptimized: true, // Görsellerin optimize edilmesi devre dışı bırakılır
   },
+  experimental: {
+    runtime: 'edge', // Varsayılan olarak Edge Runtime kullan
+  },
 };
 
 export default nextConfig;
